1ff62726748fb71075fcda060b115a8c3cb8f777
jimmybnguyen <jimmyn9@uw.edu>
2018-05-31 12:11:34 -0700

    Add shortcode for gallery post

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$('.my-form').on('submit', function () {
+$('.gallery-form').on('submit', function () {
 	var list = [];
 	var startValue = document.getElementById("start").value;
 	var endValue = document.getElementById("end").value;
     for (var i = startValue; i <= endValue; i++) {
     	list.push(i);
     }
-	document.getElementById("result").value = list;
+	document.getElementById("result").value = "[gallery link='file' size='medium' ids= " + list.toString() + "]";
 	return false;
 });
