c325d4080b30e4368989c6a63d2512c8068dd5de
jimmybnguyen <jimmyn9@uw.edu>
2018-06-05 09:44:12 -0700

    Add article sidebar function

--- js/app.js
+++ js/app.js
@@ -8,3 +8,15 @@ $('.gallery-form').on('submit', function () {
 	document.getElementById("result").value = "[gallery link='file' size='medium' ids= " + list.toString() + "]";
 	return false;
 });
+
+$('.sidebar-form').on('submit', function () {
+	var articleLink = document.getElementById("link").value;
+	var articleTitle = document.getElementById("title").value;
+	var articleDate = document.getElementById("date").value;
+	var articleImg = document.getElementById("image").value;
+	document.getElementById("result").value = '<a href="' + articleLink + '"><img class="alignleft wp-image-25193 size-thumbnail" src="' + 
+												articleImg + '" alt="Link to article: ' + articleTitle + '" width="150" height="150" /></a><a class="widget-link" href="' + 
+												articleLink + '" target="_blank">' + articleTitle + '</a><small>' + articleDate + '</small>';
+	return false;
+});
+
